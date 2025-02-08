eb7a4e9ab6f59bcec6df017b7728fa124f73831d
SamarthBagde <bagdesamarth70@gmail.com>
2025-02-08 23:02:35 +0530

    form validation done

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/Home.css";
 
 export default function Home() {
+  const [url, setUrl] = useState("");
+  const [backHalf, setBackHalf] = useState("");
+  const onSubmit = async (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className={`main`}>
       <div className={`container`}>
-        <div className={`input-filed`}>
+        <form className={`input-filed`} onSubmit={onSubmit}>
           <div className={`title-1`}>Destination URL</div>
           <input
             className={`input-1`}
@@ -13,6 +19,12 @@ export default function Home() {
             name="urlInput"
             id="urlInput"
             placeholder="https://example.com/short-url"
+            onChange={(e) => {
+              setUrl(e.target.value);
+            }}
+            autoComplete="off"
+            required
+            pattern="https?://.*"
           />
           <div className={`title-1`}>Custom back-half (optional)</div>
           <input
@@ -21,13 +33,17 @@ export default function Home() {
             name="urlInput"
             id="urlInput"
             placeholder="Ex. XYZ"
+            onChange={(e) => {
+              setBackHalf(e.target.value);
+            }}
+            autoComplete="off"
           />
           <div className={`submit-btn-container`}>
             <button className={`submit-btn`} type="submit">
               Create link
             </button>
           </div>
-        </div>
+        </form>
         <div className={`output-filed`}>
           <div className={`heading white-text`}>Heading</div>
           <div className={`text white-text`}>text</div>
